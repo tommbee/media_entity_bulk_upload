55aa9046529c54a2f3d2a703ba107428bd580d29
Tom Bennett <t.bennett@brassagency.com>
2017-11-27 13:57:52 +0000

    Switched to PHPunit

---
+++ tests/src/Unit/FieldEntityTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\Tests\media_entity_bulk_upload\Unit;
+
+class FieldEntityTest extends \PHPUnit_Framework_TestCase {
+
+	public function testTrueIsTrue() {
+		$foo = true;
+		$this->assertTrue($foo);
+	}
+
+}
